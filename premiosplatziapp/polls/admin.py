44ad36d782872f79c298a0ccc71584f0eaadb644
chechooxd <sergiogalaz60@gmail.com>
2023-01-22 16:32:15 -0300

    final de curso django

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -1,6 +1,37 @@
 from django.contrib import admin
 from .models import Question, Choice
+from django import forms
+from django.forms.models import BaseInlineFormSet
+
+
+class ChoiceInlineFormSet(BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+        # Check that no two choices have the same text
+        texts = []
+        for form in self.forms:
+            text = form.cleaned_data.get('choice_text')
+            if text in texts:
+                raise forms.ValidationError('Choices must have unique texts.')
+            texts.append(text)
+        
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+    formset = ChoiceInlineFormSet
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
+
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,34 +1,38 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    latest_question_list = get_list_or_404(Question.objects.order_by('-pub_date')[:5])
-    context = {'latest_question_list': latest_question_list}
-    try:
-        return render(request, 'polls/index.html', context)
-    except Question.DoesNotExist:
-        return HttpResponse("No hay preguntas disponibles")
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        return render(request, 'polls/detail.html', {'question': question})
-    except Question.DoesNotExist:
-        return HttpResponse("No hay preguntas disponibles")
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        return render(request, 'polls/results.html', {'question': question})
-    except Question.DoesNotExist:
-        return HttpResponse("No hay preguntas disponibles")
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 def vote(request, question_id):
